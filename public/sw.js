de3ac18c054437f7f027cb831e6ebdd87511930b
Phenrril <matias.orset@hotmail.com>
2025-09-14 15:39:26 -0300

    better SEO

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'croma3d-v1';
+const urlsToCache = [
+  '/',
+  '/public/assets/styles.css',
+  '/public/assets/img/chroma-logo.png',
+  '/public/assets/img/img1.webp',
+  '/public/assets/img/img2.webp',
+  '/public/assets/img/img3.webp',
+  '/public/assets/img/img4.webp'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
